a65275c280363da71dc630dfe4f6a27c58a4d837
techykishan <tauleshwarthakur@gmail.com>
2021-03-04 08:48:38 +0530

    first commit

--- angry_dina.py
+++ angry_dina.py
@@ -111,7 +111,7 @@ class Dina:
         # win.blit(walkLeft[self.walkCount//3], (self.x,self.y))
         #     self.walkCount += 1
 
-    def jump(self,jumps):
+    def jump(self,jumps):git 
         if jumps >0:
             isJump = True
 
